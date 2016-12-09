67ee81f5b7d60bfa115b21fdb27b8592c94c9753
Audio Gram <audiogram@audiogram.bubsr.lan>
2016-12-10 09:42:42 +1100

    customise configs

--- settings/index.js
+++ settings/index.js
@@ -21,6 +21,7 @@ var path = require("path");
 module.exports = {
   workingDirectory: path.join(__dirname, "..", "tmp"),
   storagePath: path.join(__dirname, "..", "media"),
+  redisHost: "127.0.0.1",
   fonts: [
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Regular.ttf") },
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Light.ttf"), weight: 300 },
